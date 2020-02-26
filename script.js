838e0b2081319d8175617366643ce494dca5c8b4
eric-bartell <31963754+eric-bartell@users.noreply.github.com>
2020-02-25 23:10:14 -0600

    form validator

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function OpenForm(){
   document.getElementById("formbutton").style.height = "0";
   document.getElementById("formbutton").style.display = "none"; 
 }
+function validate(){
+  if(document.forms["signup"]["name"].value == "")
+  {
+  	alert("Missing name");
+  	return false;
+  }
+  else if(document.forms["signup"]["email"].value == "")
+  {
+  	alert("Missing email");
+  	return false;
+  }
+  else if(document.forms["signup"]["date"].value == "")
+  {
+  	alert("Missing birth date");
+  	return false;
+  }
+  return true;	  		
+}
