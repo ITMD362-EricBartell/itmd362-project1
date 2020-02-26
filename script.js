7e20ad66c447304a2ee142b0814462dfb16f7a8c
eric-bartell <31963754+eric-bartell@users.noreply.github.com>
2020-02-25 21:33:21 -0600

    making text appear

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function OpenForm(){
   document.getElementById("inputform").style.height ="500px";
-  document.getElementById("inputform").style.display ="none";
+  document.getElementById("inputform").style.display ="block";
 }
