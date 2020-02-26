671b78aad36c9c5dc30019ccf40ef916b87d0185
eric-bartell <31963754+eric-bartell@users.noreply.github.com>
2020-02-25 22:04:41 -0600

    adjusting line and styles

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function OpenForm(){
   document.getElementById("inputform").style.height = "300px";
+  document.getElementById("inputform").scrollIntoView();
   document.getElementById("formbutton").style.height = "0";
+  document.getElementById("formbutton").style.display = "none"; 
 }
